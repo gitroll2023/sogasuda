e252adc73298e9c509637a2d2274b4d563917929
Choi Woojin <class2023.05@gmail.com>
2025-03-11 01:15:40 +0900

    Add Google Search Console verification meta tag

--- app/layout.js
+++ app/layout.js
@@ -23,6 +23,9 @@ export const metadata = {
       },
     ],
   },
+  verification: {
+    google: 'UEFedNALtESw4sbZOz8g0EZA-7rPXxcNOpeKGCwV9Fg',
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -33,6 +36,7 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         />
+        <meta name="google-site-verification" content="UEFedNALtESw4sbZOz8g0EZA-7rPXxcNOpeKGCwV9Fg" />
       </head>
       <body className={notoSansKr.className}>
         {children}
